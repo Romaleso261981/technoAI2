538a69dd2ce4af8afb8fe8b8902e43aa41742cc9
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-08-25 11:07:38 +0300

    feat: some bag fix

--- wp-content/themes/technoai/front-page.php
+++ wp-content/themes/technoai/front-page.php
@@ -3,13 +3,17 @@
 
 
 <!-- start main -->
+
 <main id="main">
+
   <?php if (have_rows('blocks')) : ?>
   <?php while (have_rows('blocks')) : the_row(); ?>
   <?php get_template_part('partials/flexible-blocks/' . get_row_layout()); ?>
   <?php endwhile; ?>
   <?php endif; ?>
+
 </main>
+
 <!-- End main -->
 
 <?php get_footer(); ?>
