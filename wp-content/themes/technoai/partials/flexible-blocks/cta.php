ff2fd02a097e54e6adb8e2c0db9b7f6de5a310f4
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-08-27 14:19:28 +0300

    Refactor call-to-action section to utilize dynamic fields for title, description, and button text, enhancing maintainability and flexibility. Address recurring PHP notices in debug log regarding early translation loading for the ACF domain.

--- wp-content/themes/technoai/partials/flexible-blocks/cta.php
+++ wp-content/themes/technoai/partials/flexible-blocks/cta.php
@@ -1,15 +1,21 @@
+<?php
+$title = get_sub_field('cta_title') ?: 'Мої роботи';
+$description = get_sub_field('cta_description') ?: 'Ознайомтеся з моїми останніми проектами та досягненнями';
+$button_text = get_sub_field('cta_button_text') ?: 'Замовити';
+?>
+
+
+
 <section id="call-to-action" class="call-to-action">
-    <div class="container text-center aos-init aos-animate" data-aos="zoom-out">
-      <div class="row gy-4">
-        <div class="col-lg-12">
-          <h3>Let's Discuss your Projects</h3>
-          <p>
-            We pride ourselves with our ability to perform and deliver
-            results. Use the form below to discuss your project needs with
-            our team, we will get back asap
-          </p>
-          <a class="cta-btn" href="mailto:info@example.com">Conatct Us</a>
-        </div>
+  <div class="container text-center aos-init aos-animate" data-aos="zoom-out">
+    <div class="row gy-4">
+      <div class="col-lg-12">
+        <h3><?php echo $title; ?></h3>
+        <p>
+          <?php echo $description; ?>
+        </p>
+        <a class="cta-btn" href="mailto:info@example.com"><?php echo $button_text; ?></a>
       </div>
     </div>
-  </section>
+  </div>
+</section>
