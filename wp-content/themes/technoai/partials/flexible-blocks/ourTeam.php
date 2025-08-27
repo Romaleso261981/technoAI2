9efcd5ddfa895231e16cb1ff04f78bcfdc2966c9
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-08-27 15:07:37 +0300

    Refactor our team section to utilize dynamic fields for team member details, enhancing maintainability and flexibility. Update debug log with additional PHP notices regarding early translation loading for the ACF domain.

--- wp-content/themes/technoai/partials/flexible-blocks/ourTeam.php
+++ wp-content/themes/technoai/partials/flexible-blocks/ourTeam.php
@@ -1,96 +1,39 @@
+<?php
+$title = get_sub_field('our_team_title') ?: 'Our Team';
+$description = get_sub_field('our_team_description') ?: 'Meet the experts who bring our vision to life';
+$team_members = get_sub_field('our_team_members');
+?>
+
 <section id="team" class="team section-gradient">
   <div class="container" data-aos="fade-up">
     <div class="section-header text-center">
-      <h2>Our Team</h2>
+      <h2><?php echo $title; ?></h2>
       <p class="text-white">
-        Meet the experts who bring our vision to life
+        <?php echo $description; ?>
       </p>
     </div>
 
     <div class="row gy-5 justify-content-center">
-      <!-- Team Member -->
+      <?php foreach ($team_members as $member) : ?>
       <div class="col-xl-3 col-md-6 team-member" data-aos="fade-up" data-aos-delay="100">
         <div class="member-card position-relative shadow">
           <div class="member-image">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/team/team-1.jpg" alt="Jhone Bi" />
+            <?php echo wp_get_attachment_image($member['member_image'], 'full', false, array('class' => 'member-image')); ?>
             <div class="overlay-gradient"></div>
           </div>
           <div class="member-info p-4 text-center">
-            <h4>Jhone Bi</h4>
-            <span>Application Manager</span>
+            <h4><?php echo $member['member_name']; ?></h4>
+            <span><?php echo $member['member_position']; ?></span>
             <div class="social-links mt-3">
-              <a href="#" class="social-icon"><i class="bi bi-twitter-x"></i></a>
-              <a href="#" class="social-icon"><i class="bi bi-facebook"></i></a>
-              <a href="#" class="social-icon"><i class="bi bi-linkedin"></i></a>
-              <a href="#" class="social-icon"><i class="bi bi-instagram"></i></a>
+              <?php foreach ($member['member_social_links'] as $link) : ?>
+              <a href="<?php echo $link['link']; ?>" class="social-icon"><i
+                  class="bi bi-<?php echo $link['icon']; ?>"></i></a>
+              <?php endforeach; ?>
             </div>
           </div>
         </div>
       </div>
-      <!-- End Team Member -->
-
-      <!-- Team Member -->
-      <div class="col-xl-3 col-md-6 team-member" data-aos="fade-up" data-aos-delay="200">
-        <div class="member-card position-relative shadow">
-          <div class="member-image">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/team/team-2.jpg" alt="Sani Awesome" />
-            <div class="overlay-gradient"></div>
-          </div>
-          <div class="member-info p-4 text-center">
-            <h4>Sani Awesome</h4>
-            <span>Social Media</span>
-            <div class="social-links mt-3">
-              <a href="#" class="social-icon"><i class="bi bi-twitter-x"></i></a>
-              <a href="#" class="social-icon"><i class="bi bi-facebook"></i></a>
-              <a href="#" class="social-icon"><i class="bi bi-linkedin"></i></a>
-              <a href="#" class="social-icon"><i class="bi bi-instagram"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- End Team Member -->
-
-      <!-- Team Member -->
-      <div class="col-xl-3 col-md-6 team-member" data-aos="fade-up" data-aos-delay="300">
-        <div class="member-card position-relative shadow">
-          <div class="member-image">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/team/team-3.jpg" alt="Andrio Willi" />
-            <div class="overlay-gradient"></div>
-          </div>
-          <div class="member-info p-4 text-center">
-            <h4>Andrio Willi</h4>
-            <span>Content Writer</span>
-            <div class="social-links mt-3">
-              <a href="#" class="social-icon"><i class="bi bi-twitter-x"></i></a>
-              <a href="#" class="social-icon"><i class="bi bi-facebook"></i></a>
-              <a href="#" class="social-icon"><i class="bi bi-linkedin"></i></a>
-              <a href="#" class="social-icon"><i class="bi bi-instagram"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- End Team Member -->
-
-      <!-- Team Member -->
-      <div class="col-xl-3 col-md-6 team-member" data-aos="fade-up" data-aos-delay="400">
-        <div class="member-card position-relative shadow">
-          <div class="member-image">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/team/team-4.jpg" alt="Afa Jonson" />
-            <div class="overlay-gradient"></div>
-          </div>
-          <div class="member-info p-4 text-center">
-            <h4>Afa Jonson</h4>
-            <span>Business Manager</span>
-            <div class="social-links mt-3">
-              <a href="#" class="social-icon"><i class="bi bi-twitter-x"></i></a>
-              <a href="#" class="social-icon"><i class="bi bi-facebook"></i></a>
-              <a href="#" class="social-icon"><i class="bi bi-linkedin"></i></a>
-              <a href="#" class="social-icon"><i class="bi bi-instagram"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- End Team Member -->
+      <?php endforeach; ?>
     </div>
   </div>
 </section>
