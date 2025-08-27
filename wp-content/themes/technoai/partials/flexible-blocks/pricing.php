41815d746b7594730c164ff0431dd976f3d25e4d
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-08-27 14:14:25 +0300

    Refactor pricing section to utilize dynamic fields for titles, descriptions, and pricing cards. Simplify card rendering by iterating through subfields for features and prices. Address recurring PHP notices in debug log regarding early translation loading for the ACF domain.

--- wp-content/themes/technoai/partials/flexible-blocks/pricing.php
+++ wp-content/themes/technoai/partials/flexible-blocks/pricing.php
@@ -1,107 +1,45 @@
+<?php
+$title = get_sub_field('pricing_title') ?: 'Мої роботи';
+$description = get_sub_field('pricing_description') ?: 'Ознайомтеся з моїми останніми проектами та досягненнями';
+$pricingCards = get_sub_field('pricing_cards');
+?>
+
 <section id="pricing" class="pricing section">
   <div class="container-fluid">
     <div class="container">
       <div class="section-header text-center" data-aos="fade-up">
-        <h2>Pricing Plans</h2>
+        <h2><?php echo $title; ?></h2>
         <p>
-          Choose the plan that suits your needs and start your journey
-          with us.
+          <?php echo $description; ?>
         </p>
       </div>
       <div class="row gy-4 justify-content-center">
         <!-- Pricing Cards -->
+        <?php if ($pricingCards) : ?>
+        <?php foreach ($pricingCards as $card) : ?>
         <div class="col-lg-4 col-md-12" data-aos="fade-up" data-aos-delay="100">
           <div class="pricing-card modern-card">
             <div class="card-header">
-              <h3>Basic</h3>
+              <h3><?php echo $card['pricing_card_title'] ?: 'Basic'; ?></h3>
               <div class="price">
-                <h4><sup>$</sup>25</h4>
+                <h4><sup><?php echo $card['pricing_union'] ?: '$'; ?></sup><?php echo $card['price'] ?: '25'; ?></h4>
                 <p>/month</p>
               </div>
             </div>
             <ul class="features">
+              <?php if ($card['price_features']) : ?>
+              <?php foreach ($card['price_features'] as $feature) : ?>
               <li>
-                <i class="bi bi-check-circle-fill"></i> Unlimited GB Space
+                <i class="bi bi-check-circle-fill"></i> <?php echo $feature['feature']; ?>
               </li>
-              <li>
-                <i class="bi bi-check-circle-fill"></i> 30 Domain Names
-              </li>
-              <li><i class="bi bi-check-circle-fill"></i> Free SSL</li>
-              <li>
-                <i class="bi bi-check-circle-fill"></i> Daily Backup
-              </li>
-              <li class="cross-line">
-                <i class="bi bi-x-circle-fill"></i> Free Templates
-              </li>
-              <li class="cross-line">
-                <i class="bi bi-x-circle-fill"></i> Live Support
-              </li>
+              <?php endforeach; ?>
+              <?php endif; ?>
             </ul>
-            <a href="#" class="btn-get-started">Order Now</a>
+            <a href="#" class="btn-get-started">Замовити</a>
           </div>
         </div>
-
-        <div class="col-lg-4 col-md-12" data-aos="fade-up" data-aos-delay="200">
-          <div class="pricing-card modern-card highlighted">
-            <div class="card-header">
-              <h3>Standard</h3>
-              <div class="price">
-                <h4><sup>$</sup>50</h4>
-                <p>/month</p>
-              </div>
-            </div>
-            <ul class="features">
-              <li>
-                <i class="bi bi-check-circle-fill"></i> Unlimited GB Space
-              </li>
-              <li>
-                <i class="bi bi-check-circle-fill"></i> 30 Domain Names
-              </li>
-              <li><i class="bi bi-check-circle-fill"></i> Free SSL</li>
-              <li>
-                <i class="bi bi-check-circle-fill"></i> Daily Backup
-              </li>
-              <li>
-                <i class="bi bi-check-circle-fill"></i> Free Templates
-              </li>
-              <li class="cross-line">
-                <i class="bi bi-x-circle-fill"></i> Live Support
-              </li>
-            </ul>
-            <a href="#" class="btn-get-started">Order Now</a>
-          </div>
-        </div>
-
-        <div class="col-lg-4 col-md-12" data-aos="fade-up" data-aos-delay="300">
-          <div class="pricing-card modern-card premium">
-            <div class="card-header">
-              <h3>Premium</h3>
-              <div class="price">
-                <h4><sup>$</sup>100</h4>
-                <p>/month</p>
-              </div>
-            </div>
-            <ul class="features">
-              <li>
-                <i class="bi bi-check-circle-fill"></i> Unlimited GB Space
-              </li>
-              <li>
-                <i class="bi bi-check-circle-fill"></i> 30 Domain Names
-              </li>
-              <li><i class="bi bi-check-circle-fill"></i> Free SSL</li>
-              <li>
-                <i class="bi bi-check-circle-fill"></i> Daily Backup
-              </li>
-              <li>
-                <i class="bi bi-check-circle-fill"></i> Free Templates
-              </li>
-              <li>
-                <i class="bi bi-check-circle-fill"></i> Live Support
-              </li>
-            </ul>
-            <a href="#" class="btn-get-started">Order Now</a>
-          </div>
-        </div>
+        <?php endforeach; ?>
+        <?php endif; ?>
       </div>
     </div>
   </div>
