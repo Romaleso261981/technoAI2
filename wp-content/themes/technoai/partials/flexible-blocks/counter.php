d36a0f762f5a130a539105bff5f19d618e34ec97
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-08-29 14:41:09 +0300

    Refactor counter section to utilize dynamic fields for counter data, enhancing maintainability and flexibility. Update rendering logic to correctly handle counter attributes and improve the display of labels and descriptions. Address recurring PHP notices in debug log regarding early translation loading for the ACF domain.

--- wp-content/themes/technoai/partials/flexible-blocks/counter.php
+++ wp-content/themes/technoai/partials/flexible-blocks/counter.php
@@ -1,62 +1,31 @@
+<?php
+$counters = get_sub_field('counters');
+?>
+
 <section id="stats-counter" class="stats-counter section">
   <div class="container">
     <div class="row g-4 align-items-center justify-content-center">
+      <?php foreach ($counters as $counter) : ?>
       <!-- Stat Item -->
       <div class="col-lg-3 col-md-6">
         <div class="stats-box" data-aos="zoom-in" data-aos-delay="100">
           <div class="stats-layer"></div>
           <div class="stats-icon">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/happy-clients.svg"
-              alt="Happy Clients" />
+            <?php echo wp_get_attachment_image($counter['icon'], 'full', false, array('class' => 'img-counter')); ?>
           </div>
           <div class="stats-content">
-            <h3 class="stats-number purecounter" data-purecounter-start="0" data-purecounter-end="232"
-              data-purecounter-duration="1">
-              232
+            <h3 class="stats-number purecounter" data-purecounter-start="0"
+              data-purecounter-end="<?php echo $counter['count']; ?>" data-purecounter-duration="1">
+              <?php echo $counter['count']; ?>
             </h3>
-            <p class="stats-label">Happy Clients</p>
-            <small>Consequuntur quae diredo</small>
+            <p class="stats-label">
+              <?php echo $counter['llabe']; ?>
+            </p>
+            <small><?php echo $counter['description']; ?></small>
           </div>
         </div>
       </div>
-
-      <!-- Stat Item -->
-      <div class="col-lg-3 col-md-6">
-        <div class="stats-box" data-aos="zoom-in" data-aos-delay="200">
-          <div class="stats-layer"></div>
-          <div class="stats-icon">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/complete-projects.svg"
-              alt="Completed Projects" />
-          </div>
-          <div class="stats-content">
-            <h3 class="stats-number purecounter" data-purecounter-start="0" data-purecounter-end="521"
-              data-purecounter-duration="1">
-              521
-            </h3>
-            <p class="stats-label">Completed Projects</p>
-            <small>Adipisci atque quia aut</small>
-          </div>
-        </div>
-      </div>
-
-      <!-- Stat Item -->
-      <div class="col-lg-3 col-md-6">
-        <div class="stats-box" data-aos="zoom-in" data-aos-delay="300">
-          <div class="stats-layer"></div>
-          <div class="stats-icon">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/hours-support.svg"
-              alt="Hours of Support" />
-          </div>
-          <div class="stats-content">
-            <h3 class="stats-number purecounter" data-purecounter-start="0" data-purecounter-end="453"
-              data-purecounter-duration="1">
-              453
-            </h3>
-            <p class="stats-label">Hours of Support</p>
-            <small>Aut commodi quaerat</small>
-          </div>
-        </div>
-      </div>
+      <?php endforeach; ?>
     </div>
   </div>
 </section>
